66c7fe2dd3d97ec08e787f529450111aa1dde8cd
Maggie von Ebers <vonebersm@gmail.com>
2019-02-13 09:33:09 -0600

    Continuing to set up game state

--- Homework 2/hw2pr3.py
+++ Homework 2/hw2pr3.py
@@ -11,31 +11,14 @@
 import numpy as np
 
 class Node:
-    def __init__(self):
-        self.state = tuple()
+    def __init__(self, board):
+        self.state = board
         self.parent = None
         self.children = []
 
-    def get_moves(self, board) -> list:
-        moves = []
-        if self.state[0] + 1 < 8 and self.state[1] + 1 < 8:
-            if board[self.state[0] + 1][self.state[1] + 1] == 1:
-                moves.append((self.state[0] + 1, self.state[1] + 1))
-        if self.state[0] + 1 < 8 and self.state[1] - 1 >= 0:
-            if board[self.state[0] + 1][self.state[1] - 1] == 1:
-                moves.append((self.state[0] + 1, self.state[1] - 1))
-        if self.state[0] - 1 >= 0 and self.state[1] - 1 >= 0:
-            if board[self.state[0] - 1][self.state[1] - 1] == 1:
-                moves.append((self.state[0] - 1, self.state[1] - 1))
-        if self.state[0] - 1 >= 0 and self.state[1] + 1 < 8:
-            if board[self.state[0] - 1][self.state[1] + 1] == 1:
-                moves.append((self.state[0] - 1, self.state[1] + 1))
-
-        return moves
-
-
-    def result(self, action):
-        return self.children[action]
+    def result(self, pos, action):
+        if action in self.state.get_moves(pos):
+            return 1
 
     def test(self):
         return len(self.children) == 0
@@ -46,28 +29,92 @@ class Node:
 class Game:
     def __init__(self):
         self.board = np.zeros((8,8))
-        for i in range(8):
-            for j in range(8):
-                if (i + j) % 2 != 0:
-                    self.board[i][j] = 1
+
         self.board[7][6] = 2
         self.board[0][1] = -1
         self.board[0][3] = -1
         self.board[0][5] = -1
         self.board[0][7] = -1
         print(self.board)
-        self.rams_pos = [(0,1), (0,3), (0,5), (0,7)]
         self.tom_pos = (7,6)
+        self.num_to_board = {}
+        ind1 = 7
+        ind2 = 6
+        #for i in range(1,33):
+         #   if ind1 % 2 == 0:
+          #      ind2
+
+        self.num_to_board = {
+            1 : (7,6),
+            2 : (7,4),
+            3 : (7,2),
+            4 : (7,0),
+            5 : (6,7),
+            6 : (6,5),
+            7 : (6,3),
+            8 : (6,1),
+            9 : (5,6),
+            10 : (5,4),
+            11 : (5,2),
+            12 : (5,0),
+            13 : (4,7),
+            14 : (4,5),
+            15 : (4,3),
+            16 : (4,1),
+            17 : (3,6),
+            18 : (3,4),
+            19 : (3,2),
+            20 : (3,0),
+            21 : (2,7),
+            22 : (2,5),
+            23 : (2,3),
+            24 : (2,1),
+            25 : (1,6),
+            26 : (1,4),
+            27 : (1,2),
+            28 : (1,0),
+            29 : (0,7),
+            30 : (0,5),
+            31 : (0,3),
+            32 : (0,1)
+        }
+
+
+    def get_moves(self, pos: int) -> list:
+        new_pos = self.num_to_board[pos]
+        moves = []
+        if new_pos[0] + 1 < 8 and new_pos[1] + 1 < 8:
+            if self.board[new_pos[0] + 1][new_pos[1] + 1] != -1:
+                moves.append((new_pos[0] + 1, new_pos[1] + 1))
+        if new_pos[0] + 1 < 8 and new_pos[1] - 1 >= 0:
+            if self.board[new_pos[0] + 1][new_pos[1] - 1] != -1:
+                moves.append((new_pos[0] + 1, new_pos[1] - 1))
+        if new_pos[0] - 1 >= 0 and new_pos[1] - 1 >= 0:
+            if self.board[new_pos[0] - 1][new_pos[1] - 1] != -1:
+                moves.append((new_pos[0] - 1, new_pos[1] - 1))
+        if new_pos[0] - 1 >= 0 and new_pos[1] + 1 < 8:
+            if self.board[new_pos[0] - 1][new_pos[1] + 1] != -1:
+                moves.append((new_pos[0] - 1, new_pos[1] + 1))
+
+        return moves
+
 
     def move(self, pos: int, move: int) -> dict: # return the new board
-        return
+        if self.num_to_board[move] in self.get_moves(pos):
+            m1 = self.num_to_board[move][0]
+            m2 = self.num_to_board[move][1]
+            p1 = self.num_to_board[pos][0]
+            p2 = self.num_to_board[pos][1]
+            self.board[m1][m2] = self.board[p1][p2]
+            self.board[p1][p2] = 0
+        return self.board
 
 
-    def is_finished(self, tom: Node) -> bool:
-        #if self.brady_pos in [29, 30, 31, 32]:
-         #   return True
+    def is_finished(self, tom: int) -> bool:
+        if tom in [29, 30, 31, 32]:
+            return True
        
-        if len(tom.get_moves(self.board)) == 0:
+        if len(self.get_moves(tom)) == 0:
             return True
 
         return False
@@ -85,7 +132,7 @@ def max_value(n: Node, game: Game) -> Node:
     if n.test() or game.is_finished():
         return n.utility()
 
-    v = Node()
+    v = Node(game.board)
     v.state = -10000
 
     for a in range(len(n.children)):
@@ -100,7 +147,7 @@ def min_value(n: Node, game: Game) -> Node:
     if n.test() or game.is_finished():
         return n.utility
 
-    v = Node()
+    v = Node(game.board)
     v.state = 10000
 
     for a in range(len(n.children)):
