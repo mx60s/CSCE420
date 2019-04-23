a4a87a3d08f8980fb4e21340c7073ff906d9b795
Maggie von Ebers <vonebersm@gmail.com>
2019-04-23 16:43:18 -0500

    bdf

--- Project/ugh.py
+++ Project/ugh.py
@@ -2,25 +2,32 @@
 
 import numpy as np
 import random
+import math
 
 class BackPropNet:
 
     def __init__(self, examples):
         self.examples = examples
-        self.network = [[0] * len(examples[0][0]), [0] * 30, [0] * 2]
-        self.weights = [[0] * (len(examples[0][0]) * 30), [0] * 60]
-        self.deltas = [[0] * len(examples[0][0]), [0] * 30, [0] * 2]
-        self.sums = [[0] * len(examples[0][0]), [0] * 30, [0] * 2]
+        self.network = [[0] * len(examples[0][0]), [0] * 5, [0]]
+        self.weights = [[0] * (len(examples[0][0]) * 5), [0] * 5]
+        self.deltas = [[0] * len(examples[0][0]), [0] * 5, [0]]
+        self.sums = [[0] * len(examples[0][0]), [0] * 5, [0]]
         self.eqs = [None, lambda x : np.tanh(x), lambda x : 1/(1+np.exp(-x))]
+        self.deriv = [ None, 
+                        lambda x : 1/(np.cosh(x)**2), 
+                        lambda x : self.eqs[2](x) * (1 - self.eqs[2](x)) ]
 
 
     def learn(self):
         for i in range(len(self.weights)):
             for j in range(len(self.weights[i])):
-                self.weights[i][j] = random.random() / 5
+                self.weights[i][j] = random.random()
         
-        for _ in range(24):  # 3 epochs
+        for e in range(7):  # epochs
+            print("Epoch #", e)
+            error = []
             for x, y in self.examples:
+
                 self.network[0] = x
 
                 for i in range(1, len(self.network)):   # for each layer
@@ -28,28 +35,40 @@ class BackPropNet:
                         sum_w = 0
                         mult = len(self.network[i])
                         for k in range(len(self.network[i - 1])):   #for each of the previous nodes
-                            sum_w += self.weights[i-1][k*mult] * self.network[i-1][k]
+                            sum_w = sum_w + (self.weights[i - 1][k * mult + j] * self.network[i - 1][k])
+
                         self.network[i][j] = self.eqs[i](sum_w)
                         self.sums[i][j] = sum_w
                 
                 """ Backpropagation """
                 for i in range(len(self.network[2])):   # for each node in output layer
-                    self.deltas[2][i] = self.eqs[1](self.sums[2][i]) * (y - self.network[2][i])
+                    self.deltas[2][i] = self.deriv[2](self.sums[2][i]) * (y - self.network[2][i])
 
                 for i in range(len(self.network) - 2, 1, -1):
                     for j in range(len(self.network[i])):
                         sums_d = 0
                         mult = len(self.network[i])
                         for k in range(len(self.network[i + 1])):
-                            sum_d += self.weights[i][k * mult] * self.deltas[i + 1][k]
-                        self.deltas[i][j] = 0.5 * self.sums[i][j] * sums_d
+                            sum_d = sum_d + self.weights[i][k * mult + j] * self.deltas[i + 1][k]
+                        self.deltas[i][j] = self.deriv[i](self.sums[i][j]) * sums_d   # could be an issue with the equation
+                
+                for i in range(len(self.weights)):
+                    for j in range(len(self.weights[i])):
+                        n = int(math.floor(j / len(self.network[i + 1])))
+                        d = int(math.floor(j / len(self.network[i])))
+                        self.weights[i][j] = self.weights[i][j] + (0.9 * self.network[i][n] * self.deltas[i+1][d])
 
-                print(*self.network[2])
-                    
+                error.append(self.deltas[2])
+
+            print("Avg error:", np.mean(error))
+        
 
 
-training_set = [([1,1,1], 0), ([1,1,1], 1), ([1,1,0], 1), ([1,0,0], 0), ([0,1,1], 1), ([1,0,1], 1),
-                ([0,1,0], 0), ([0,0,1], 0)]
+training_set = [([1,1,1,1,1], 1), ([1,1,1,1,0], 1), ([1,1,1,0,1], 1), ([1,1,0,1,1], 1), ([1,0,1,1,1], 1),
+                ([0,0,0,0,1], 0), ([0,0,1,0,1], 0), ([0,1,0,0,1], 0), ([0,0,0,0,0], 0), ([0,0,0,0,1], 0),
+                ([0,1,1,1,1], 1), ([1,1,1,0,0], 1), ([1,1,0,0,1], 1), ([1,0,0,1,1], 1), ([0,0,1,1,1], 1),
+                ([0,1,1,1,0], 1), ([0,1,1,0,1], 1), ([0,1,0,1,1], 1), ([0,0,0,1,1], 0), ([0,0,1,1,0], 0),
+                ([1,0,0,0,0], 0), ([1,0,1,1,0], 1)]
 
 net = BackPropNet(training_set)
 net.learn()
